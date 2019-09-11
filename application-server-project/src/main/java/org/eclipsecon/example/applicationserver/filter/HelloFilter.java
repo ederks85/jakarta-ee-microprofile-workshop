803fa75c6a71e11dfa6d3c80f69f87c23fec5a16
Edwin Derks <ederks85@gmail.com>
2019-09-11 10:04:24 +0200

    Initial setup of application-server-project

---
+++ application-server-project/src/main/java/org/eclipsecon/example/applicationserver/filter/HelloFilter.java
@@ -0,0 +1,23 @@
+package org.eclipsecon.example.applicationserver.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class HelloFilter implements Filter {
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setAttribute("message",	"Hello message from filter");
+		
+		chain.doFilter(request, response);
+	}
+}
---
+++ application-server-project/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Application Server example: Payara 5.193</title>
+</head>
+<body>
+	<h1>Hello from the other side... eh Payara</h1>
+	<pre>Message received:</pre>
+	<p>${message}</p>
+</body>
+</html>
