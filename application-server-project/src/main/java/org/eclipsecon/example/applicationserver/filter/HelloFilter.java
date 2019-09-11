dbb7fbf2218f0fbd303f4e6b755ad59cc7ffe455
Edwin Derks <ederks85@gmail.com>
2019-09-11 11:19:22 +0200

    - Update artifact names after builds - Add hollow-jar-example project

--- application-server-project/src/main/java/org/eclipsecon/example/applicationserver/filter/HelloFilter.java
+++ application-server-project/src/main/java/org/eclipsecon/example/applicationserver/filter/HelloFilter.java
@@ -1,9 +1,9 @@
 package org.eclipsecon.example.applicationserver.filter;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
---
+++ hollow-jar-project/src/main/java/org/eclipsecon/example/hollowjar/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package org.eclipsecon.example.hollowjar.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+
+}
