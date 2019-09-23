4aee3c76b60d5a87192b3d77bba85932fe27fe57
Edwin Derks <ederks85@gmail.com>
2019-09-23 14:38:31 +0200

    Implement JSF configuration and initial example

---
+++ application-server-project/src/main/java/org/eclipsecon/example/applicationserver/JSFConfiguration.java
@@ -0,0 +1,11 @@
+package org.eclipsecon.example.applicationserver;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+
+@ApplicationScoped
+@FacesConfig(version = Version.JSF_2_3)
+public class JSFConfiguration {
+
+}
