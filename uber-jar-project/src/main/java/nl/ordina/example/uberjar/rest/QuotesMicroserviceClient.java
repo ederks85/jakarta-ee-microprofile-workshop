3ce746a4c0604d77b2a9e78c1c6da7260661bab2
Edwin Derks <edwin.derks@ordina.nl>
2019-09-27 16:45:07 +0200

    WIP Add functionality to retrieve Quotes via the intermediary hollow JAR service

---
+++ uber-jar-project/src/main/java/nl/ordina/example/uberjar/rest/QuotesMicroserviceClient.java
@@ -0,0 +1,20 @@
+package nl.ordina.example.uberjar.rest;
+
+import nl.ordina.example.uberjar.domain.Quote;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/quotes")
+@Produces(APPLICATION_JSON)
+@RegisterRestClient
+public interface QuotesMicroserviceClient {
+
+    @GET
+    @Path("/random")
+    Quote getRandomQuoteFromMicroservice();
+}
